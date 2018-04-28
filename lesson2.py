94240d11b7af7cec24652fb487b9d4786d567f87
JuliFed <fedorchenkojuli@gmail.com>
2018-04-28 12:23:57 +0300

    code style

--- lesson2.py
+++ lesson2.py
@@ -1,23 +1,24 @@
-#Comment
-def hello(*names,**kwargs):
+# Comment
+def hello(*names, **kwargs):
     for name in names:
         print(name)
+
 
 def hello2(*names):
     it = iter(names)
     while True:
         try:
             x = next(it)
-            print("Hello "+x)
+            print("Hello " + x)
         except StopIteration:
             return
+
 
 """Comment
     many
         strings"""
-
-#hello("fasdf","sdgfas",1,2,3,4,5)
-#hello2('1',"sdfsf","sdfsdfg","dfgsdfg")
+# hello("fasdf","sdgfas",1,2,3,4,5)
+# hello2('1',"sdfsf","sdfsdfg","dfgsdfg")
 
 """Типы данных в Python  
     bool, int, float
@@ -27,41 +28,41 @@ def hello2(*names):
 
 # list,tuple
 x = 123
-y = ["sss","aaa","qqqq"]
-z = ("a","b","c")
+y = ["sss", "aaa", "qqqq"]
+z = ("a", "b", "c")
 
-print(x,id(x))
-print(y,id(y))
-print(z,id(z))
+print(x, id(x))
+print(y, id(y))
+print(z, id(z))
 print()
 x += 5
 y += [5]
 z += (5,)
-print(x,id(x))
-print(y,id(y))
-print(z,id(z))
-
+print(x, id(x))
+print(y, id(y))
+print(z, id(z))
 
 # значения передаются по ссылке
 print()
 x = 123
+
+
 def foo(x):
     print(id(x))
+
 
 print(id(x))
 foo(x)
 
-
 # dict
 x = {
-    123:123,
-    '123':124,
-    "dddd":"ddd",
+    123: 123,
+    '123': 124,
+    "dddd": "ddd",
 }
 
 print(x["123"])
 print(x[123])
-
 
 print("Hello %s" % "user")
 print("Hello %d" % 1022)
@@ -71,7 +72,6 @@ print("%.2f" % 0.1)
 # разобраться с байтами! bytes
 print(b'fgsdfg'[1])
 print("привет".encode('utf-8'))
-
-
-
-
+print("привет".encode('cp1251'))
+b = "привет".encode('cp1251')
+print(b.decode('cp1251'))
