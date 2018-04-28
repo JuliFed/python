a2ead4ede9e1e193427a0226f36f0f3cf7d0487e
python <fedorchenkojuli@gmail.com>
2018-04-28 11:27:15 +0300

    lesson2 continue

--- lesson2.py
+++ lesson2.py
@@ -16,11 +16,37 @@ def hello2(*names):
     many
         strings"""
 
-hello("fasdf","sdgfas",1,2,3,4,5)
-hello2('1',"sdfsf","sdfsdfg","dfgsdfg")
+#hello("fasdf","sdgfas",1,2,3,4,5)
+#hello2('1',"sdfsf","sdfsdfg","dfgsdfg")
 
 """Типы данных в Python  
-    bool
-    int
-    float 
+    bool, int, float
+    
+    list, tuple 
 """
+
+# list,tuple
+x = 123
+y = ["sss","aaa","qqqq"]
+z = ("a","b","c")
+
+print(x,id(x))
+print(y,id(y))
+print(z,id(z))
+print()
+x += 5
+y += [5]
+z += (5,)
+print(x,id(x))
+print(y,id(y))
+print(z,id(z))
+
+
+# значения передаются по ссылке
+print()
+x = 123
+def foo(x):
+    print(id(x))
+
+print(id(x))
+foo(x)
