47a791f64828e2ec92fcc3c84e3a775c20abf92f
JuliFed <fedorchenkojuli@gmail.com>
2018-04-28 13:31:25 +0300

    code style

--- lesson2.py
+++ lesson2.py
@@ -64,14 +64,93 @@ x = {
 print(x["123"])
 print(x[123])
 
+# string format
 print("Hello %s" % "user")
 print("Hello %d" % 1022)
 print("Hello %02d" % 1)
 print("%.2f" % 0.1)
 
 # разобраться с байтами! bytes
+# разобраться с utf-8
 print(b'fgsdfg'[1])
 print("привет".encode('utf-8'))
 print("привет".encode('cp1251'))
 b = "привет".encode('cp1251')
 print(b.decode('cp1251'))
+
+# string, tuple, list, bytes
+print("assdffghhjj"[2:5])
+print("assdffghhjj"[:3])
+print("assdffghhjj"[::2])
+
+w = range(10, 20, 2)
+# w = range(10)
+for i in w:
+    print(i, end=' ')
+
+print(1 in w)
+
+# def foo(a, b, *args, c = b, **kwargs)
+
+
+def foo(a, b, c="abc"):
+    print(a,b,c)
+
+
+foo(1, 2, 3)
+foo(1, 2)
+
+
+def test(a, g, *, c="x"):
+    print(a, g, c)
+
+
+# test(2, 3, 4)
+# test(2, 3)
+
+print("kwd", test.__kwdefaults__)
+test.__kwdefaults__["c"] = "zzz"
+print("kwd", test.__kwdefaults__)
+
+
+# Задачки
+
+def fact(n):
+    if n == 0:
+        return 0
+    return n * fact(n-1)
+
+
+print("Fact 0:", fact(0))
+print("Fact 5:", fact(5))
+
+
+def fib(n):
+    result = []
+    a, b = 0, 1
+    result.append(a)
+    result.append(b)
+    for i in range(2, n):
+        x = a + b
+        result.append(x)
+        a, b = b, x
+    return result
+
+
+print(fib(5))
+print(fib(20))
+
+
+def fib_gen(n):
+    if n <= 1:
+        return 0
+    yield 0
+    yield 1
+    a, b = 0, 1
+    for i in range(2, n):
+        a, b = b, a + b
+        yield b
+
+
+for i in fib_gen(20):
+    print(i, end=', ')
