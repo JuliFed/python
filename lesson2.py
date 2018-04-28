b44b07b8c733ff98cb739ab9c19a3b978a11ab84
python <fedorchenkojuli@gmail.com>
2018-04-28 10:56:27 +0300

    lesson2 continue

--- lesson2.py
+++ lesson2.py
@@ -1,5 +1,26 @@
+#Comment
 def hello(*names,**kwargs):
     for name in names:
         print(name)
 
+def hello2(*names):
+    it = iter(names)
+    while True:
+        try:
+            x = next(it)
+            print("Hello "+x)
+        except StopIteration:
+            return
+
+"""Comment
+    many
+        strings"""
+
 hello("fasdf","sdgfas",1,2,3,4,5)
+hello2('1',"sdfsf","sdfsdfg","dfgsdfg")
+
+"""Типы данных в Python  
+    bool
+    int
+    float 
+"""
