1f14c6749822fdb6a7fa9924c3d883125e87d100
JuliFed <fedorchenkojuli@gmail.com>
2018-05-12 11:17:07 +0300

    Added class Property - setter, getter, deleter

--- lesson5.py
+++ lesson5.py
@@ -1,6 +1,75 @@
-import argparse
+# import argparse
+#
+# parser = argparse.ArgumentParser(description='Process some integers.')
+# parser.add_argument('--field_size', '-f', metavar='-f', type=int, help='Size of game field')
+# args = parser.parse_args()
+# print('Field size =', args.field_size)
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--field_size', '-f', metavar='-f', type=int, help='Size of game field')
-args = parser.parse_args()
-print('Field size =',args.field_size)
+
+class Bar:
+    _x = 0
+
+    @property
+    def foo(self):
+        return self._x
+
+    @foo.setter
+    def foo(self, value):
+        self._x = value ** 2
+
+    @foo.deleter
+    def foo(self):
+        del self._x
+
+
+# bar = Bar()
+# print(bar.foo)
+# bar.foo = 2
+# print(bar.foo)
+# del bar.foo
+# print(bar.foo)
+
+
+class Property:
+    def __init__(self, getter, setter, deleter):
+        self.g = getter
+        self.s = setter
+        self.d = deleter
+
+    def __get__(self, obj, cls=None):
+        if self.g is None:
+            raise AttributeError
+        return self.g(obj)
+
+    def __set__(self, obj, value):
+        if self.s is None:
+            raise AttributeError
+        self.s(obj, value)
+
+    def __delete__(self, obj):
+        if self.d is None:
+            raise AttributeError
+        self.d(obj)
+
+
+class Bar2:
+    _x = None
+
+    def getx(self):
+        return self._x
+
+    def setx(self, value):
+        self._x = value ** 2
+
+    def delx (self):
+        del self._x
+
+    foo = Property(getx, setx, delx)
+
+
+bar2 = Bar2()
+print(bar2.foo)
+bar2.foo = 25
+print(bar2.foo)
+del bar2.foo
+print(bar2.foo)
